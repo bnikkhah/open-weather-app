492162defcf55f0bb838aa1661a1deb7c1ee0cd9
Behnam Nikkhah <bnikkhah22@gmail.com>
2021-09-16 23:52:34 -0600

    add redux devtools to store

--- store/index.js
+++ store/index.js
@@ -1,6 +1,6 @@
 import { createStore } from "redux"
 import rootReducer from '../reducers/index'
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 export default store
